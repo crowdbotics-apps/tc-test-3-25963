df9253b033036e5369f6c2d2dac4026bc013d9de
Ben Goldstein <ben@bens-mbp-3.home>
2021-04-27 10:43:02 -0700

    remove summernote from admin for testing plaintext

--- backend/modules/terms/admin.py
+++ backend/modules/terms/admin.py
@@ -1,21 +1,21 @@
-from django_summernote.admin import SummernoteModelAdmin
-from django.contrib import admin
-
-# Register your models here.
-from .models import TermAndCondition
-
-
-# admin.site.register(TermAndCondition)
-
-
-class TermAndConditionAdmin(SummernoteModelAdmin):
-    summernote_fields = ('body')
-
-admin.site.register(TermAndCondition, TermAndConditionAdmin)
-
-
+# from django_summernote.admin import SummernoteModelAdmin
 # from django.contrib import admin
+
+# # Register your models here.
 # from .models import TermAndCondition
 
-# admin.site.register(TermAndCondition)
+
+# # admin.site.register(TermAndCondition)
+
+
+# class TermAndConditionAdmin(SummernoteModelAdmin):
+#     summernote_fields = ('body')
+
+# admin.site.register(TermAndCondition, TermAndConditionAdmin)
+
+
+from django.contrib import admin
+from .models import TermAndCondition
+
+admin.site.register(TermAndCondition)
 
